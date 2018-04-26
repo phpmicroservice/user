0cce69c5389102a6b6805ca658d6c2ade26ae73b
Dongasai <1514582970@qq.com>
2018-04-26 11:06:48 +0800

    小修改

--- app/logic/User.php
+++ app/logic/User.php
@@ -41,44 +41,7 @@ class User extends \app\Base
 
     }
 
-    public static function in_role($roles)
-    {
-        $roles_user = self::role();
 
-        if (is_array($roles)) {
-            foreach ($roles as $role) {
-                if (array_key_exists($role, $roles_user)) {
-                    return $role;
-                }
-            }
-
-        } else {
-            if (array_key_exists($roles, $roles_user)) {
-                return $roles;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * 读取当前用户的权限列表
-     * @return array
-     */
-    public static function role()
-    {
-        $session = Di::getDefault()->getShared('session');
-        $roles = $session->get('roles' . self::is_login());
-        if (1) {
-            # 没能读取用户的 角色信息
-            # 读取一下
-            $roles = \logic\rbac\Role::user(self::is_login());
-            # 增加一个游客权限
-            $roles['visitor'] = 0;
-            $session = Di::getDefault()->getShared('session');
-            $session->set('roles' . self::is_login(), $roles);
-        }
-        return $roles;
-    }
 
 
     /**
@@ -92,6 +55,7 @@ class User extends \app\Base
         if (!$validation->validate(['user_id' => $user_id, 'username' => $new_username, 'edit_username' => 0])) {
             return $validation->getMessage();
         }
+
 
         # 验证通过
         $userModel = \app\model\user::findFirstById($user_id);
